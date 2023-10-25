c99e22398a961e08ca63f175928250a030a319fa
Arnold Kibira <arnold.ril3y@gmail.com>
2023-10-25 22:31:36 +0300

    add colours

---
+++ constants/colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  purpleDark: "#2536A7",
+  purplePrimary: "##475AD7",
+  purpleLight: "#8A96E5",
+  purpleLighter: "#EEF0FB",
+  blackDark: "#22242F",
+  blackPrimary: "#333647",
+  blackLight: "#44485F",
+  blackLighter: "#555A77",
+  grayDark: "#666C8E",
+  grayPrimary: "#7C82A1",
+  grayLight: "#ACAFC3",
+  grayLighter: "#F3F4F6",
+};
